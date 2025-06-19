9f13d0a9b204d82cd038eece40159334dcf3098b
RobertWesner <robert@wesner.io>
2025-06-19 15:02:37 +0200

    Added more fun to 1337.php, because why bother at this point

--- routes/1337.php
+++ routes/1337.php
@@ -20,7 +20,28 @@ use RobertWesner\SimpleMvcPhp\Route;
     ) {
         /** @var callable<Route::get> $routing */
         $routing($route, function () {
-            return Route::response('<b>You\'re so close. Try harder!</b>');
+            switch (random_int(0, 9)) {
+                case 0:
+                    return Route::response('<b>You\'re so close. Try harder!</b>');
+                case 1:
+                    return Route::response('<b>Slow down, cowboy!</b>');
+                case 2:
+                    return Route::response('<b>Wowzers, another blank page?</b>');
+                case 3:
+                    return Route::response('<b>Get a job</b>');
+                case 4:
+                    return Route::response('<b>Interesting...</b>');
+                case 5:
+                    return Route::response('<b>Mate, this website is FOSS anyway. Just read the source code.</b>');
+                case 6:
+                    return Route::response('<b>Hmmm? Did you say something?</b>', random_int(400, 599));
+                case 7:
+                    return Route::response('<b>It\'s dangerous to go alone, fuck off!</b>');
+                case 8:
+                    return Route::response('<b>404? I know what you are doing...</b>', 404);
+                case 9:
+                    return Route::response('<h1 style="color: red">Internal Server Error</h1><b>But no, it isn\'t... you just tried some stupid things.</b>', 500);
+            }
         });
     }
 })();
