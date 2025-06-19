8cd6bad907405ba0df06d20356e07e0e5ca308d6
RobertWesner <robert@wesner.io>
2025-06-19 14:53:56 +0200

    Added more fun to 1337.php

--- routes/1337.php
+++ routes/1337.php
@@ -7,7 +7,7 @@ declare(strict_types=1);
 use RobertWesner\SimpleMvcPhp\Route;
 
 (function () {
-    $route = /** @lang RegExp */ '(/@vite/env|/actuator/env|/php-cgi.*|/server|/\.vscode/sftp\.json|/debug/default/view\?panel=config|/v2/_catalog|/ecp/Current/exporttool/.*|/login\.action|/_all_dbs|/\.DS_Store|/.env|/\.git/config|/config\.json|/telescope/requests|/info\.php|/\?rest_route=.*|/wordpress/.*)';
+    $route = /** @lang RegExp */ '(/@vite/env|/actuator/env|/php-cgi.*|/server|/\.vscode/sftp\.json|/debug/default/view\?panel=config|/v2/_catalog|/ecp/Current/exporttool/.*|/login\.action|/_all_dbs|/\.DS_Store|/.env|/\.git/config|/config\.json|/telescope/requests|/info\.php|/\?rest_route=.*|/wordpress/.*|/blog/.*|/web/.*|/website/.*|/news/.*|/20\d{2}/.*|/shop/.*|/wp\d/.*|/test/.*|/media/.*|/cms/.*|/sito/.*)';
 
     foreach (
         [
